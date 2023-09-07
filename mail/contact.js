ac2612a336f3ea254061eb5a41abdee3ac6cc6aa
pmojeda <pmojeda@gmail.com>
2023-09-07 13:33:30 -0300

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "https://formspree.io/f/mrgwkrkq",
                 type: "POST",
                 data: {
                     name: name,
